2e2c1fcf605c4967557449256cacd4bd5f6b3c2a
Jim Barrows <Jim.Barrows@gmail.com>
2020-10-08 23:02:19 -0500

    initial

---
+++ src/main/java/org/erpmicroservices/accounting_and_budgeting/endpoint/rest/accounting_and_budgetingapirest/AccountingAndBudgetingApiRestApplication.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.accounting_and_budgeting.endpoint.rest.accounting_and_budgetingapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingAndBudgetingApiRestApplication {
+
+ public static void main(String[] args) {
+	SpringApplication.run(AccountingAndBudgetingApiRestApplication.class, args);
+ }
+
+}
---
+++ src/test/java/org/erpmicroservices/accounting_and_budgeting/endpoint/rest/accounting_and_budgetingapirest/AccountingAndBudgetingApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.erpmicroservices.accounting_and_budgeting.endpoint.rest.accounting_and_budgetingapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingAndBudgetingApiRestApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
