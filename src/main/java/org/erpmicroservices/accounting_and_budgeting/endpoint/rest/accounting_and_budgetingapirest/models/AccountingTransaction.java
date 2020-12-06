a514ca84c067b99f2f192cf85bc6fa1ae2a8aeca
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-05 18:52:56 -0600

    Accounting Transactions complete

---
+++ src/main/java/org/erpmicroservices/accounting_and_budgeting/endpoint/rest/accounting_and_budgetingapirest/models/AccountingTransaction.java
@@ -0,0 +1,32 @@
+package org.erpmicroservices.accounting_and_budgeting.endpoint.rest.accounting_and_budgetingapirest.models;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@ToString
+@EqualsAndHashCode
+public class AccountingTransaction extends AbstractPersistable<UUID> {
+ @NotNull
+ private LocalDate transactionDate;
+ private LocalDate entryDate = LocalDate.now();
+ @NotBlank
+ private String description;
+ @ManyToOne
+ private AccountingTransactionType type;
+ private UUID partyRoleId;
+ private UUID partyId;
+ private UUID invoiceId;
+ private UUID paymentId;
+ private UUID inventoryItemVarianceId;
+}
