d46dc82b6a126eb3ade9247b2c25b396811582a3
Jim Barrows <Jim.Barrows@gmail.com>
2020-12-11 10:27:10 -0600

    GL account associations and subsidiary ledger accounts.

--- src/main/java/org/erpmicroservices/accounting_and_budgeting/endpoint/rest/accounting_and_budgetingapirest/models/OrganizationGlAccount.java
+++ src/main/java/org/erpmicroservices/accounting_and_budgeting/endpoint/rest/accounting_and_budgetingapirest/models/OrganizationGlAccount.java
@@ -6,7 +6,10 @@ import lombok.ToString;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,5 +21,12 @@ public class OrganizationGlAccount extends AbstractPersistable<UUID> {
  private LocalDate fromDate;
  private LocalDate thruDate;
  private UUID internalOrganizationId;
+ private UUID supplierId;
+ private UUID billToCustomerId;
+ private UUID productCategoryId;
+ private UUID productId;
+ @OneToMany
+ @JoinColumn(name = "organization_gl_account_id")
+ private List<OrganizationGlAccount> organizationGlAccountList;
 
 }
